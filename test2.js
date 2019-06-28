13e898249badc32c3bde28319c8df184b5db2b5b
alauhy <39290088+alauhy@users.noreply.github.com>
2019-06-28 10:32:09 +0800

    add arrayoperation.js

---
+++ test2.js
@@ -0,0 +1,71 @@
+
+
+// 阅读推荐的链接，复习JavaScript中数组的相关方法
+
+// 写程序判断下列变量是不是数组类型。
+var a = '[a, b, c, d]';
+var b = [1, 2, 3, 4];
+//TODO
+console.log(a instanceof Array);
+console.log(b instanceof Array);
+
+// 编写程序，将下面数组中的每一项都乘以2。
+var a = [1, 2, 3, 4, 5];
+// TODO should output [2,4,6,8,10]
+for(var i in a){
+    a[i]=a[i]<<2;
+}
+console.log(a);
+// 编写程序，按下面的要求输出结果。
+var colors = ["Red", "Green", "White", "Black"];
+//TODO case 1 output: 'Red Green White Black'
+// case 2 output: 'Red+Green+White+Black'
+// case 3 output: 'Red,Green,White,Black'
+
+var str=colors.join(' ');
+var str1= colors.join('+');
+var str2= colors.join(',');
+
+console.log(str);
+console.log(str1);
+console.log(str2);
+// 编写程序，将下面数组中的数字按从大到小的顺序排序。
+var a = [5, 1, 8, 10, 4];
+//TODO should output: [10,8,5,4,1]
+for(var i=0 ;i<a.length;i++){
+    for(var j=i+1;j<a.length;j++){
+        if(a[i]<a[j]){
+            var temp = a[i];
+            a[i]=a[j];
+            a[j]=temp;
+        }
+    }
+}
+
+console.log(a);
+
+// 编程程序，找出下列数组中出现频率最高的元素。
+
+var a = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+//TODO should output: 'a'
+var b=[3,'a',2,4,9];
+var c=[];
+
+for(var i in b){
+    var count=0;
+    for(var j in a){
+        if(b[i]==a[j]){
+            count++;
+        }
+    }
+    c.push(count);
+}
+var tmp=c[0];
+var positon=0;
+for(var i in c){
+    if(tmp<c[i]){
+        tmp=c[i];
+        positon=i;
+    }
+}
+console.log(b[positon]);
