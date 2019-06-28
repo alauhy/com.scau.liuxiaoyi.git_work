0530cd9287a528bb00c426b743c52887ffadb2bc
alauhy <39290088+alauhy@users.noreply.github.com>
2019-06-28 09:39:43 +0800

    update test1

--- test1.js
+++ test1.js
@@ -4,32 +4,27 @@
 // 完成下面程序，将`name`变量中的字母全部转为为大写，输出：`'HELLO'`。
 var name = 'hello';
 // TODO write your code here...
-
+let str = name.toUpperCase();
+    console.log(str);
 
 // 完成下面程序，将sentence变量中的所有单词首字母变为大写，输出：'Good Afternoon, Mr Mike.'。
 var sentence = 'good afternoon, mr mike.';
 //TODO write your code here...
-
+let str =sentence.split(' ');
+let str2 ='';
+for(var i in str){
+    var char=str[i].charAt(0);
+    str[i]=str[i].replace(char,function replace(char){
+        return char.toUpperCase();
+    })
+    str2+=str[i]+" ";
+}
+console.log(str2);
 
 // 完成下面程序，将money变量中的金额提取出来，输出：20。
 var money = '￥20';
 //TODO write your code here...
 
-function upcaseAll(){
-    let str = name.toUpperCase();
-    console.log(str);
-}
-function firstToUpcase(){
-    let str =sentence.split(' ');
-    let str2 ='';
-    for(var i in str){
-        str[i].charAt(0).toUpperCase();
-        str2+=str[i];
-    }
-    console.log(str2);
-    
-}
-function getMoney(){
-    let str = money.split('￥');
-    console.log(str);
-}
+let str = money.split('￥');
+console.log(str[1]);
+
